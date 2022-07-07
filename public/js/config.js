8cd2046a5cb8aa6367cff3021abf0883535aabb6
LemonPi24 <48165233+LemonPi24@users.noreply.github.com>
2022-07-07 17:08:40 -0400

    Create config.js

---
+++ public/js/config.js
@@ -0,0 +1,5 @@
+// https://github.com/webtutsplus/videoChat-WebFrontend/blob/master/README.md
+// https://xirsys.com/
+turnConfig = {
+   iceServers: [{   urls: [ "stun:us-turn3.xirsys.com" ]}, {   username: "S8i4st_imrxZXLvEFhVW3uuNzEjQhQiSUkgpRaNRf8dpyL-lZacT9MslAWVoGJX_AAAAAGLF4jNtYXh3YW5n",   credential: "b7ae2dc4-fd61-11ec-b107-0242ac140004",   urls: [       "turn:us-turn3.xirsys.com:80?transport=udp",       "turn:us-turn3.xirsys.com:3478?transport=udp",       "turn:us-turn3.xirsys.com:80?transport=tcp",       "turn:us-turn3.xirsys.com:3478?transport=tcp",       "turns:us-turn3.xirsys.com:443?transport=tcp",       "turns:us-turn3.xirsys.com:5349?transport=tcp"   ]}]
+}
